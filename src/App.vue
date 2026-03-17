aea1914b00fbded06a3bacb5e03fe6a29fc9e589
Wanderson PJ <wandersonpj_bkn@hotmail.com>
2026-03-17 17:27:39 -0300

    chore: adds robots block

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,13 @@
 
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
+import { useHead } from '@unhead/vue'
+
 import AppHeader from '@/components/layout/AppHeader.vue'
 import AppFooter from '@/components/layout/AppFooter.vue'
+
+useHead({
+  htmlAttrs: { lang: 'pt-BR' },
+  meta: [{ name: 'robots', content: 'noindex, nofollow' }],
+})
 </script>
